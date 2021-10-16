671fe254a76b3544b9ef94c42ebc6eee325da59d
Muhammad Ganjar Imanudin <ganjar_ibrahimovic@rocketmail.com>
2021-10-16 22:55:28 +0700

    test homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # Root
+  root to: "homepage#index"
+
+  devise_for :users
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
